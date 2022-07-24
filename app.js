940346e07d770098a876d5f6ae03acd29af7663e
shSalman009 <shahsalmanahmed009@gmail.com>
2022-07-24 16:41:24 +0600

    added modal to read full about project

--- app.js
+++ app.js
@@ -43,8 +43,53 @@ function addProject() {
         btnWrapper.className = "buttons";
         btnOne.className = "button";
         btnTwo.className = "button";
+        p.className = "stop-line";
 
         projects.appendChild(project);
+
+        // Modal
+        const modal = document.querySelector(".modal");
+        const out = document.querySelector(".out");
+
+        const addFunction = () => {
+            const cloneProject = project.cloneNode(true);
+            const detectPara = cloneProject.lastChild.firstChild.nextSibling;
+
+            const lastButton = cloneProject.lastChild.lastChild.lastChild;
+            lastButton.innerText = "Read Less";
+            lastButton.addEventListener("click", () => {
+                outFunction();
+            });
+
+            if (document.body.classList.contains("light")) {
+                document.body.classList.add("lightShape");
+            }
+            if (document.body.classList.contains("dark")) {
+                document.body.classList.add("darkShape");
+            }
+            detectPara.classList.remove("stop-line");
+
+            cloneProject.setAttribute("id", "modal-body");
+            modal.style.display = "block";
+            modal.appendChild(cloneProject);
+        };
+
+        const outFunction = () => {
+            const detectChild = document.getElementById("modal-body");
+            if (detectChild) {
+                modal.removeChild(detectChild);
+                modal.style.display = "none";
+            }
+        };
+        btnTwo.addEventListener("click", addFunction);
+
+        window.addEventListener("mouseup", function (event) {
+            if (!event.target.closest("#modal-body")) {
+                outFunction();
+            }
+        });
+
+        out.addEventListener("click", outFunction);
     });
 }
 addProject();
@@ -59,7 +104,7 @@ let open = document.getElementById("mobile-open");
 let close = document.getElementById("mobile-close");
 let darkImgs = document.querySelectorAll(".darkImg");
 let lightImgs = document.querySelectorAll(".lightImg");
-
+const modal = document.querySelector(".modal");
 // const toggleBtn = document.getElementById("toggle-btn");
 
 // Responsive Breakpoints
@@ -79,6 +124,7 @@ function theme() {
     darkImgs.forEach((img) => {
         img.classList.add("d-none");
     });
+    modal.classList.add("light");
 
     checkbox.addEventListener("change", () => {
         if (
@@ -93,6 +139,8 @@ function theme() {
             header.classList.add("light");
             nav.classList.remove("dark");
             nav.classList.add("light");
+            modal.classList.remove("dark");
+            modal.classList.add("light");
             // image change
             darkImgs.forEach((img) => {
                 img.classList.add("d-none");
@@ -112,6 +160,8 @@ function theme() {
             header.classList.add("dark");
             nav.classList.remove("light");
             nav.classList.add("dark");
+            modal.classList.remove("light");
+            modal.classList.add("dark");
             // image change
             lightImgs.forEach((img) => {
                 img.classList.add("d-none");
