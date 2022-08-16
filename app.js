28fb01cd52fddb058d1e780b7535540090d9075d
Salman Ahmed <shahsalmanahmed009@gmail.com>
2022-08-16 22:12:45 +0600

    added new one project

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 // more
 
 function addProject() {
-    const projects = document.querySelector(".project-wrapper");
-    data.map((d) => {
-        // Creat
-        const project = document.createElement("div");
-        const left = document.createElement("div");
-        const right = document.createElement("div");
-        const lightImg = document.createElement("img");
-        const darkImg = document.createElement("img");
-        const h4 = document.createElement("h4");
-        const p = document.createElement("p");
-        const btnWrapper = document.createElement("div");
-        const btn = document.createElement("button");
-        const link = document.createElement("a");
+  const projects = document.querySelector(".project-wrapper");
+  data.map((d) => {
+    // Creat
+    const project = document.createElement("div");
+    const left = document.createElement("div");
+    const right = document.createElement("div");
+    const lightImg = document.createElement("img");
+    const darkImg = document.createElement("img");
+    const h4 = document.createElement("h4");
+    const p = document.createElement("p");
+    const btnWrapper = document.createElement("div");
+    const btn = document.createElement("button");
+    const link = document.createElement("a");
 
-        // Including1
-        project.append(left, right);
-        left.append(lightImg, darkImg);
-        right.append(h4, p, btnWrapper);
-        btnWrapper.append(btn);
-        btn.appendChild(link);
+    // Including1
+    project.append(left, right);
+    left.append(lightImg, darkImg);
+    right.append(h4, p, btnWrapper);
+    btnWrapper.append(btn);
+    btn.appendChild(link);
 
-        // add values
-        h4.textContent = d.title;
-        p.textContent = d.description;
-        lightImg.src = d.img[0];
-        darkImg.src = d.img[1];
-        link.href = d.link;
-        link.target = "_blank";
-        link.innerText = "Visit Website";
+    // add values
+    h4.textContent = d.title;
+    p.innerHTML = d.description;
+    lightImg.src = d.img[0];
+    darkImg.src = d.img[1];
+    link.href = d.link;
+    link.target = "_blank";
+    link.innerText = "Visit Website";
 
-        // add css
+    // add css
 
-        project.className = "project";
-        left.className = "project-left";
-        right.className = "project-right";
-        lightImg.className = "lightImg";
-        darkImg.className = "darkImg";
-        btnWrapper.className = "buttons";
-        btn.className = "button";
+    project.className = "project";
+    left.className = "project-left";
+    right.className = "project-right";
+    lightImg.className = "lightImg";
+    darkImg.className = "darkImg";
+    btnWrapper.className = "buttons";
+    btn.className = "button";
 
-        projects.appendChild(project);
-    });
+    projects.appendChild(project);
+  });
 }
 addProject();
 
@@ -61,118 +61,122 @@ let lightImgs = document.querySelectorAll(".lightImg");
 
 // Responsive Breakpoints
 window.addEventListener("resize", (e) => {
-    window
-        .matchMedia("(max-width: 768px)")
-        .addEventListener("change", (e) => (md = e.matches));
+  window
+    .matchMedia("(max-width: 768px)")
+    .addEventListener("change", (e) => (md = e.matches));
 });
 
 // Light and dark mode function
 function theme() {
-    document.body.classList.add("light");
-    banner.classList.add("light");
-    header.classList.add("light");
-    icons.forEach((icon) => icon.classList.add("lightIcon"));
-    nav.classList.add("light");
-    darkImgs.forEach((img) => {
-        img.classList.add("d-none");
-    });
+  document.body.classList.add("light");
+  banner.classList.add("light");
+  header.classList.add("light");
+  icons.forEach((icon) => icon.classList.add("lightIcon"));
+  nav.classList.add("light");
+  darkImgs.forEach((img) => {
+    img.classList.add("d-none");
+  });
 
-    checkbox.addEventListener("change", () => {
-        if (
-            document.body.classList.contains("dark") &&
-            !document.body.classList.contains("light")
-        ) {
-            document.body.classList.remove("dark");
-            document.body.classList.add("light");
-            banner.classList.remove("dark");
-            banner.classList.add("light");
-            header.classList.remove("dark");
-            header.classList.add("light");
-            nav.classList.remove("dark");
-            nav.classList.add("light");
-            // image change
-            darkImgs.forEach((img) => {
-                img.classList.add("d-none");
-            });
-            lightImgs.forEach((img) => {
-                img.classList.remove("d-none");
-            });
-        } else if (
-            !document.body.classList.contains("dark") &&
-            document.body.classList.contains("light")
-        ) {
-            document.body.classList.add("dark");
-            document.body.classList.remove("light");
-            banner.classList.add("dark");
-            banner.classList.remove("light");
-            header.classList.remove("light");
-            header.classList.add("dark");
-            nav.classList.remove("light");
-            nav.classList.add("dark");
-            // image change
-            lightImgs.forEach((img) => {
-                img.classList.add("d-none");
-            });
-            darkImgs.forEach((img) => {
-                img.classList.remove("d-none");
-            });
-        }
-        icons.forEach((icon) => icon.classList.toggle("darkIcon"));
-    });
+  checkbox.addEventListener("change", () => {
+    if (
+      document.body.classList.contains("dark") &&
+      !document.body.classList.contains("light")
+    ) {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+      banner.classList.remove("dark");
+      banner.classList.add("light");
+      header.classList.remove("dark");
+      header.classList.add("light");
+      nav.classList.remove("dark");
+      nav.classList.add("light");
+      // image change
+      darkImgs.forEach((img) => {
+        img.classList.add("d-none");
+      });
+      lightImgs.forEach((img) => {
+        img.classList.remove("d-none");
+      });
+    } else if (
+      !document.body.classList.contains("dark") &&
+      document.body.classList.contains("light")
+    ) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+      banner.classList.add("dark");
+      banner.classList.remove("light");
+      header.classList.remove("light");
+      header.classList.add("dark");
+      nav.classList.remove("light");
+      nav.classList.add("dark");
+      // image change
+      lightImgs.forEach((img) => {
+        img.classList.add("d-none");
+      });
+      darkImgs.forEach((img) => {
+        img.classList.remove("d-none");
+      });
+    }
+    icons.forEach((icon) => icon.classList.toggle("darkIcon"));
+  });
 }
 theme();
 
 // mobile nav toggler function
 function navToggle() {
-    if (
-        !open.classList.contains("d-none") &&
-        close.classList.contains("d-none")
-    ) {
-        open.classList.add("d-none");
-        close.classList.remove("d-none");
-        nav.classList.remove("close");
-        nav.classList.add("open");
-        if (document.body.classList.contains("light")) {
-            document.body.classList.add("lightShape");
-            banner.classList.add("lightShape");
-        } else if (document.body.classList.contains("dark")) {
-            document.body.classList.add("darkShape");
-            banner.classList.add("darkShape");
-        }
-    } else if (
-        open.classList.contains("d-none") &&
-        !close.classList.contains("d-none")
-    ) {
-        open.classList.remove("d-none");
-        close.classList.add("d-none");
-        nav.classList.remove("open");
-        nav.classList.add("close");
-        document.body.classList.remove("darkShape");
-        document.body.classList.remove("lightShape");
-        banner.classList.remove("darkShape");
-        banner.classList.remove("lightShape");
+  if (
+    !open.classList.contains("d-none") &&
+    close.classList.contains("d-none")
+  ) {
+    open.classList.add("d-none");
+    close.classList.remove("d-none");
+    nav.classList.remove("close");
+    nav.classList.add("open");
+    if (document.body.classList.contains("light")) {
+      document.body.classList.add("lightShape");
+      banner.classList.add("lightShape");
+    } else if (document.body.classList.contains("dark")) {
+      document.body.classList.add("darkShape");
+      banner.classList.add("darkShape");
     }
+  } else if (
+    open.classList.contains("d-none") &&
+    !close.classList.contains("d-none")
+  ) {
+    open.classList.remove("d-none");
+    close.classList.add("d-none");
+    nav.classList.remove("open");
+    nav.classList.add("close");
+    document.body.classList.remove("darkShape");
+    document.body.classList.remove("lightShape");
+    banner.classList.remove("darkShape");
+    banner.classList.remove("lightShape");
+  }
 }
 navButton.addEventListener("click", navToggle);
 
 // click outside to close mobile nav
 window.addEventListener("mouseup", function (event) {
+  if (
+    !event.target.closest("#navbar") &&
+    !event.target.closest(".mobile-nav")
+  ) {
     if (
-        !event.target.closest("#navbar") &&
-        !event.target.closest(".mobile-nav")
+      open.classList.contains("d-none") &&
+      !close.classList.contains("d-none")
     ) {
-        if (
-            open.classList.contains("d-none") &&
-            !close.classList.contains("d-none")
-        ) {
-            open.classList.remove("d-none");
-            close.classList.add("d-none");
-            nav.classList.remove("open");
-            nav.classList.add("close");
-        }
-        document.body.classList.remove("darkShape");
-        document.body.classList.remove("lightShape");
-        banner.classList.remove("darkShape");
-        banner.classList.remove("lightShape");
+      open.classList.remove("d-none");
+      close.classList.add("d-none");
+      nav.classList.remove("open");
+      nav.classList.add("close");
     }
+    document.body.classList.remove("darkShape");
+    document.body.classList.remove("lightShape");
+    banner.classList.remove("darkShape");
+    banner.classList.remove("lightShape");
+  }
 });
+
+const mmm = document.getElementById("mytext");
+
+console.log(mmm.substr(0, 20));
--- data.js
+++ data.js
@@ -1,18 +1,26 @@
 const data = [
-    {
-        id: 1,
-        title: "Online Course Sell application",
-        img: ["./img/online-course-light.png", "./img/online-course-dark.png"],
-        description:
-            "This is a fully functional react online course sell application. For created this website i used react-bootstrap along with react js for front-end and firebase as backend. This website is also responsive for all device.",
-        link: "https://online-school-knowmore.netlify.app",
-    },
-    {
-        id: 2,
-        title: "E-Commerce",
-        img: ["./img/e-commerce-light.png", "./img/e-commerce-dark.png"],
-        description:
-            "This is an E-Commerce onlie shopping website. In this website you can add, remove, update, clear products from cart. you can purchase your cart items. you can filter product or can visit single product details.",
-        link: "https://fantastic-starlight-bf0953.netlify.app",
-    },
+  {
+    id: 1,
+    title: "Online Course Sell application",
+    img: ["./img/online-course-light.png", "./img/online-course-dark.png"],
+    description:
+      "This is a fully functional react online course sell application. For created this website i used react-bootstrap along with react js for front-end and firebase as backend. This website is also responsive for all device.",
+    link: "https://online-school-knowmore.netlify.app",
+  },
+  {
+    id: 2,
+    title: "E-Commerce",
+    img: ["./img/e-commerce-light.png", "./img/e-commerce-dark.png"],
+    description:
+      "This is an E-Commerce onlie shopping website. In this website you can add, remove, update, clear products from cart. you can purchase your cart items. you can filter product or can visit single product details.",
+    link: "https://fantastic-starlight-bf0953.netlify.app",
+  },
+  {
+    id: 3,
+    title: "IT Company",
+    img: ["./img/company-light.png", "./img/company-dark.png"],
+    description:
+      "This is a simple static IT service company website. <br/> <strong>Used Language</strong> <br/><ul><li><span>HTML</span></li><li><span>CSS</span></li><li><span>javaScript</span></li></ul> ",
+    link: "https://salmans-brand.netlify.app",
+  },
 ];
