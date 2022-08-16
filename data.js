336c04ab195b1555d7f9f8147114433549892d53
Salman Ahmed <shahsalmanahmed009@gmail.com>
2022-08-16 22:36:52 +0600

    added a new skill and updated profile picture

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ const data = [
     title: "E-Commerce",
     img: ["./img/e-commerce-light.png", "./img/e-commerce-dark.png"],
     description:
-      "This is an E-Commerce onlie shopping website. In this website you can add, remove, update, clear products from cart. you can purchase your cart items. you can filter product or can visit single product details.",
+      "This is an E-Commerce onlie shopping website. In this website you can add, remove, update, clear products from cart. you can purchase your cart items. you can filter product and can visit single product details.",
     link: "https://fantastic-starlight-bf0953.netlify.app",
   },
   {
